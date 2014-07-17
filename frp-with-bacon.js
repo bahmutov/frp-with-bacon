f01c4f10751e796ca72a586a7b350019e9a17fa4
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-07-17 14:08:30 +0100

    Adds basic example of creating Bacon event stream from web socket
    We create two filtered streams from the main stream, and use one of them
    to display text in d3. The other one we currently just log to the console.

---
+++ frp-with-bacon.js
@@ -0,0 +1,88 @@
+"use strict";
+
+var pageData = [];
+
+var width = 960,
+    height = 500;
+
+var svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(32," + (height / 2) + ")");
+
+function update(data) {
+    var text = svg.selectAll("text").data(data);
+
+    // UPDATE
+    // Update old elements as needed.
+    text.attr("class", "update")
+        .transition()
+        .duration(550)
+        .style("fill-opacity", 1e-6)
+        .transition()
+        .duration(550)
+        .style("fill-opacity", 1)
+        .text(function (d) { return d; });
+    
+    // ENTER
+    // Create new elements as needed.
+    var enterText = svg.selectAll("text").data(data).enter()
+        .append("text")
+        .attr("class", "enter")
+        .attr("dy", ".35em")
+        .attr("y", -60)
+        .style("fill-opacity", 1e-6)
+        .text(function (d) { return d; })
+        .transition()
+        .duration(750)
+        .attr("y", 0)
+        .style("fill-opacity", 1);
+
+    // EXIT
+    // Remove old elements as needed.
+    text.exit()
+        .attr("class", "exit")
+        .transition()
+        .duration(750)
+        .attr("y", 60)
+        .style("fill-opacity", 1e-6)
+        .remove();
+}
+
+// The initial display.
+update(pageData);
+
+// Create our websocket to get wiki updates
+var ws = new WebSocket("ws://wiki-update-sockets.herokuapp.com/");
+ws.onopen = function () {
+    console.log("Connection opened");
+};
+
+ws.onclose = function () {
+    console.log("Connection is closed...");
+};
+
+var messageStream = Bacon.fromEventTarget(ws, "message").map(function(event) {
+    var dataString = event.data;
+    return JSON.parse(dataString);
+});
+
+// Filter the message stream for newuser events
+var newUserStream = messageStream.filter(function(message) {
+    return message.type === "newuser";
+});
+newUserStream.onValue(function(results) {
+    console.log(JSON.stringify(results));
+});
+
+// Filter the message stream for unspecified events, which we're taking to mean edits in this case
+var editStream = messageStream.filter(function(message) {
+    return message.type === "unspecified";
+});
+editStream.onValue(function(results) {
+    pageData = [JSON.stringify(results)];
+    update(pageData);
+});
+
+// TODO: Calculate the rate of edits per second
